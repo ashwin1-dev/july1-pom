ad38d2a4e92f99f336ec8923df1dd228a0e7a6c0
root <root@srvr1.dev.edu>
2021-08-01 04:24:18 +0000

    testing maven

---
+++ src/main/java/com/finale/prj1/App.java
@@ -0,0 +1,14 @@
+package com.finale.prj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Settinga...  vengai ka beta akela khada hai" );
+    }
+}
